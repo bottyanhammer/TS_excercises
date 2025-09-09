b24c7d174a1a098a64be3d42a820d2bcabd45dd4
bottyanhammer <bottyan.hammer@gmail.com>
2025-09-09 08:43:51 +0200

    nullish_operator

--- src/app.ts
+++ src/app.ts
@@ -14,17 +14,19 @@ const setNumberList2 = (count: number, v:number, createNumber: (v:number) => num
     return Array.from({length: count}, item => createNumber(v) );
 }
 
-const numbers = setNumberList2(100, 5, createNumber);
+const numbers = setNumberList2(10, 5, createNumber);
 console.log(numbers);
 
 type Stat = Record<number, number>;
 const statistic: Stat = {};
 for (let v of numbers) {
-    if (v in statistic) {
+    /* if (v in statistic) {
         statistic[v]! += 1;
     } else {
         statistic[v] = 1;
-    }
+    } */
+    // nullish coalescing operátor - létezik a kulcs?
+    statistic[v] = (statistic[v] ?? 0) + 1;
 }
 console.log(statistic);
 
